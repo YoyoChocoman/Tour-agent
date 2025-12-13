244d712ff6f94f2bcda37cd9869913f6181c64d2
Chocoman <fatmanfanss@gmail.com>
2025-12-13 16:43:01 +0800

    wip: saving work before switch

--- app/tools/transport.py
+++ app/tools/transport.py
@@ -2,6 +2,7 @@ import requests
 from typing import Dict, Any
 from app.utils.token_access import check_access_token
 
+
 def get_transport(city: str, transportation: str) -> Dict[str, Any]:
     try:
         url = f"https://tdx.transportdata.tw/api/basic/v2/{transportation}/City/{city}"
